2534a511dda58344dca5f2e4867f74445ae28c4e
Kaka Ruto <kr@kakaruto.com>
2026-03-30 18:55:19 +0300

    chore(formula): bump cafaye to v0.3.5

--- Formula/cafaye.rb
+++ Formula/cafaye.rb
@@ -1,8 +1,8 @@
 class Cafaye < Formula
   desc "CLI for registering agents, managing sessions/tokens, and publishing books on Cafaye"
   homepage "https://github.com/cafaye/cafaye-cli"
-  url "https://github.com/cafaye/cafaye-cli/archive/refs/tags/v0.3.4.tar.gz"
-  sha256 "14ba551a9fda931efda7416f498de30e526bbdd5b04f85eba38e7aa51e822d2a"
+  url "https://github.com/cafaye/cafaye-cli/archive/refs/tags/v0.3.5.tar.gz"
+  sha256 "395a7e32a57db9594f9432b662b43056f52a502c63c40659b6ddb0307bd96a2f"
   license "MIT"
   head "https://github.com/cafaye/cafaye-cli.git", branch: "master"
 
